e3177c52f33d291d946c9016bb712815ead94e8e
oxgen1 <allee_jack@me.com>
2015-08-16 19:53:14 -0400

    New Website
    Better (soon)

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<link href="main.css" rel="stylesheet" type="text/css">
+<script type="text/javascript" src="main.js"></script>
+<div id="header">
+  <nav>
+    <ul class="list">
+      <li><a href="/games.html">Game Making</a></li>
+      <li><a href="/programming.php">Programming</a></li>
+      <li><a href="/index.php">Home</a></li>
+    </ul>
+    <h1 class="title">Jack's Development Profile</h1>
+  </nav>
+</div>
+<meta charset="utf-8">
+<title>Jack's Development</title>
+</head><body>
+</body>
+</html>
